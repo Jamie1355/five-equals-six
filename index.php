f55a3d16811e385e67e6ed5e8cd1ed6af9a0da1b
Jamie Canning <canningjamie@gmail.com>
2017-09-12 10:04:16 +0100

    Added index page

---
+++ index.php
@@ -0,0 +1,27 @@
+<html>
+    <head>
+	    <title>Five Equals Six</title>
+		<style>
+		    #header{
+				width:100%;
+				height:100px;
+				background:red;
+			}
+		
+		
+		</style>
+	</head>
+	
+	<body>
+	    <div id=header>
+		    This is a header
+		</div>
+		
+		<div id=sidebar>
+		    Sidebar
+		</div>
+		
+		<div id=content>
+		    Content
+		</div>
+	</body
