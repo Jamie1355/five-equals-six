be35355e7cd9c21441887909d04640cea9a35b68
Anton Leach <aleach@ICT.org.uk>
2017-09-15 11:51:28 +0100

    added JS file

---
+++ html/5e6.js
@@ -0,0 +1,16 @@
+function toggle_sidebar()
+{
+	sidebar_div = document.getElementById("sidebar");
+	content_div = document.getElementById("content");
+	if (sidebar_div.style.display == 'none')
+	{
+		sidebar_div.style.display = "block";
+		content_div.style.width="800px";
+	}else{
+		sidebar_div.style.display = "none";
+		content_div.style.width="1000px";
+	}
+	
+	
+	
+}
--- html/index.php
+++ html/index.php
@@ -2,18 +2,44 @@
     <head>
 	    <title>Five Equals Six</title>
 		<link rel="stylesheet" type="text/css" href="styles.css">
+		
+		<script type="text/javascript" src="5e6.js"></script>
+		
 	</head>
 	
 	<body>
-	    <div id=header>
-		    This is a header
-		</div>
+		<div id="wholpepage">
+			<div id=header>
+				This is a header
+				<BR>
+				<button onclick="toggle_sidebar()">-</button>
+			</div>
+			<table CELLPADDING=0 CELLSPACING=0>
+			<tr>
+			<td id="sidebar" valign="top">
 		
-		<div id=sidebar>
-		    Sidebar
-		</div>
-		
-		<div id=content>
-		    Content
+				Sidebar
+				
+				<h2>Section 1</H2>
+				<h2>Section 1</H2>
+				<h2>Section 1</H2>
+				<h2>Section 1</H2>
+				<h2>Section 1</H2>
+				<h2>Section 1</H2>
+				<h2>Section 1</H2>
+				<h2>Section 1</H2>
+				<h2>Section 1</H2>
+				<h2>Section 1</H2>
+				<h2>Section 1</H2>
+				
+			</td>
+			<td valign="top">
+			
+			<div id="content">
+				<iframe id="iframe" scrolling=auto width=100% name="content" src="topic.php"></iframe>
+			</div>
+			</td>
+			</table>
+			
 		</div>
 	</body
