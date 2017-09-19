4e28fcb65ed3221d6416be25a0dd826a56ee5e3e
Chris Riches <chrisriches42@gmail.com>
2017-09-19 09:16:32 +0100

    Added all topic 5 subtopics to insert_topics.sql

--- data/insert_topics.sql
+++ data/insert_topics.sql
@@ -1,23 +1,32 @@
 INSERT INTO Topics(TopicID, TopicName, TopicFolderPath) VALUES
-(1,'Programming',''),
-(2,'Data Structures',''),
-(3,'Algorithms',''),
-(4,'Theory of computation',''),
+(1, 'Programming',''),
+(2, 'Data Structures',''),
+(3, 'Algorithms',''),
+(4, 'Theory of computation',''),
 (5, 'Data Representation', '5fundamentalsofdatarepresentation/'),
-(6,'Computer Systems',''),
-(7,'Organisation &amp; Architecture',''),
-(8,'Consequenses of Computing',''),
-(9,'Communication &amp; Networking',''),
-(10,'Databases',''),
-(11,'Big Data',''),
-(12,'Functional Programming',''),
-(13,'System Life Cycle','');
-
-
+(6, 'Computer Systems',''),
+(7, 'Organisation &amp; Architecture',''),
+(8, 'Consequenses of Computing',''),
+(9, 'Communication &amp; Networking',''),
+(10, 'Databases',''),
+(11, 'Big Data',''),
+(12, 'Functional Programming',''),
+(13, 'System Life Cycle','');
 
 INSERT INTO SubTopics(TopicID, SubTopicName, SubTopicFolderPath) VALUES
 (5, 'Number Systems', '01numbersystems'),
 (5, 'Number Bases', '02numberbases'),
 (5, 'Units', '03units'),
-(5, 'Binary Sums', '03binarysums'),
-(5, 'Two''s Complement', '03twoscomplement');
+(5, 'Binary Sums', '04binarysums'),
+(5, 'Two''s Complement', '05twoscomplement'),
+(5, 'Binary Fractions Maths', '06binaryfractionsmaths'),
+(5, 'Binary Fractions Theory', '07binaryfractionstheory'),
+(5, 'Binary String Representation', '08binarystringrepresentation'),
+(5, 'Error Checking and Correction', '09errorchecking'),
+(5, 'Analogue vs Digital', '10analoguedigital'),
+(5, 'Bitmap Graphics', '11bitmapgraphics'),
+(5, 'Vector Graphics', '12vectorgraphics'),
+(5, 'Binary Sound Representation', '13soundrepresentation'),
+(5, 'MIDI Files', '14midi'),
+(5, 'Data Compression', '15compression'),
+(5, 'Data Encryption', '16encryption');
