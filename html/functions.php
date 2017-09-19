e860cb49255ebf0e3b22c5c25bb4f6559b29363d
Jamie Canning <canningjamie@gmail.com>
2017-09-19 16:02:09 +0100

    Started question functions.

--- html/functions.php
+++ html/functions.php
@@ -20,5 +20,41 @@
 		return $retval;
 	  }
    }
+   
+   class QuestionGetter{
+	   function QAndA(){
+		   $num = $params["numOfQuestions"];
+		   $questions = array();
+		   $answers = array();
+		   $contents = file_get_contents($params["questionsPath"]."/questions.txt");
+		   $questions[] = GetQs($contents);
+		   foreach($questions as $q){
+			   echo $q;
+		   }
+	   }
+	   
+	   function GetQs($text){
+		   $SEARCH = "###QUESTION";
+		   $qs = array();
+		   $counter = 0;
+		   $offset = 1;
+		   $previousPos = 0;
+		   
+		   while(true){
+			   $pos = strpos($text, $SEARCH, $offset);
+			   if($pos !== false){
+				   qs[$counter] = substr($text, $previousPos + strlen($SEARCH), $pos - strlen($text));
+				   $counter++;
+				   $offset = $pos + 1;
+				   $previousPos = $pos;
+			   }
+			   else{
+				   break;
+			   }
+		   }
+		   
+		   return qs;
+	   }
+   }
   
 ?>
